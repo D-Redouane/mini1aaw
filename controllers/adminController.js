bbce7004f4a03ab84ee4a0ae311b3b11d2bffdb6
D-Redouane <redouanedaddi316@gmail.com>
2024-04-24 01:17:39 +0100

    3.1 commit

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,9 +1,18 @@
-exports.getAdminPage = (req, res) => {
-  const user = localStorage.getItem('user'); // Get the user from local storage
+// Middleware to check if a user is authenticated
+const isAuthenticated = (req, res, next) => {
+  const userId = req.cookies.user; // Get the user ID from cookies
+  if (userId) {
+    req.user = { id: userId }; // Store the user ID in the request object
+    return next(); // Proceed to the next middleware
+  }
+  res.redirect('/login'); // If not authenticated, redirect to login
+};
 
-  if (user && user.loggedIn) { // Check if the user is logged in
-    res.render('admin/index');
+// Admin page route
+exports.getAdminPage = (req, res) => {
+  if (req.user) { // Check if the user information is present
+    res.render('admin/index'); // Render the admin page
   } else {
-    res.redirect('/login');
+    res.render('admin/index'); // Redirect to login if no user is found
   }
 };
--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,16 @@ const userController = require('../controllers/userController');
 const productController = require('../controllers/productController');
 const adminController = require('../controllers/adminController');
 
+const isAuthenticated = (req, res, next) => {
+  const userId = req.cookies;
+  console.log(req) // Get the user ID from cookies
+  if (userId) {
+    req.user = { id: userId }; // Store the user ID in the request object
+    return next(); // Proceed to the next middleware
+  }
+  // res.redirect('/login'); // If not authenticated, redirect to login
+};
+
 
 routes.get('/', (req, res) => {
   res.render('index');
@@ -15,24 +25,24 @@ routes.get('/login', (req, res) => {
 
 routes.post('/login', userController.loginUser);
 
-routes.get('/users', userController.getAllUsers);
-routes.post('/users', userController.createUser);
+routes.get('/users', isAuthenticated ,userController.getAllUsers);
+routes.post('/users',isAuthenticated, userController.createUser);
 
-routes.get('/admin', adminController.getAdminPage);
+routes.get('/admin', isAuthenticated, adminController.getAdminPage);
 
 
 
 
 
 routes.get('/products', productController.getAllProducts);
-routes.get('/products/new', (req, res) => {
+routes.get('/products/new',isAuthenticated, (req, res) => {
   res.render('products/new-product');
 });
-routes.post('/products/create', productController.createProduct);
+routes.post('/products/create', isAuthenticated , productController.createProduct);
 
 routes.get('/products/:id', productController.getProductById);
-routes.get('/products/:id/edit', productController.getProductEditPageById);
-routes.post('/products/:id', productController.updateProduct);
-routes.post('/products/:id/delete', productController.deleteProduct);
+routes.get('/products/:id/edit', isAuthenticated , productController.getProductEditPageById);
+routes.post('/products/:id', isAuthenticated ,productController.updateProduct);
+routes.post('/products/:id/delete', isAuthenticated ,productController.deleteProduct);
 
 module.exports = routes;
